4899f650cb27972120ddd094989da44d1988e4b2
irvandrian <96559035+irvandrian@users.noreply.github.com>
2021-12-27 13:15:18 +0700

    fix bugs5

--- components/MoviesCollection.js
+++ components/MoviesCollection.js
@@ -5,9 +5,13 @@ function MoviesCollection({ results, title }) {
     <div className="relative flex flex-col space-y-2 my-10 px-8 max-w-[1400px] mx-auto">
       <h3 className="font-semibold">{title}</h3>
       <div className="flex space-x-2 overflow-y-hidden overflow-x-scroll scrollbar-hide p-2 -m-2">
-        {results.map((result) => (
+      
+   
+        {results?.map((result) => (
           <MovieThumbnail key={result.id} result={result} />
         ))}
+        
+
       </div>
     </div>
   );
